8e652aaf2877bfe0ee40320b22c5fd16e5d85346
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2022-10-24 17:42:30 -0300

    Add files via upload

---
+++ main(1).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    float n1,n2,n3;
+    printf("digite um numero: ");
+    scanf("%f",&n1);
+    if (n1<0){
+        printf("o numero é negativo ");
+    }
+    else if (n2>0){
+        printf("o numero é positivo ");
+    }
+    else{
+    printf("o numero é neutro ");
+    }
+    return 0;
+    }
